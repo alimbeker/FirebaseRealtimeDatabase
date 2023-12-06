c5017018756989ff4c678ab13281b1d345b3f16e
Amir Alimbekerov <200103054@stu.sdu.edu.kz>
2023-12-06 18:48:15 +0600

    getReference()

--- app/src/main/java/com/example/firebaserealtimedatabase/MainActivity.kt
+++ app/src/main/java/com/example/firebaserealtimedatabase/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.firebaserealtimedatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebaserealtimedatabase.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.save.setOnClickListener {
+            val db = FirebaseDatabase.getInstance()
+            db.getReference("").setValue()
+        }
+
+
     }
 }
