0178de0cd5c70ae9d5729d3c934b9552ef45eea3
Amir Alimbekerov <200103054@stu.sdu.edu.kz>
2023-12-06 19:45:03 +0600

    FRDWrapper

--- app/src/main/java/com/example/firebaserealtimedatabase/FRDWrapper.kt
+++ app/src/main/java/com/example/firebaserealtimedatabase/FRDWrapper.kt
@@ -5,34 +5,30 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.FirebaseDatabase
 
-abstract class FRDWrapper<T> {
+abstract class FRDWrapper<T : Any> {
     private val db = FirebaseDatabase.getInstance()
 
     protected abstract fun getTableName(): String
     protected abstract fun getClassType(): Class<T>
 
-
     private val _getDataLiveData = MutableLiveData<T?>()
     val getDataLiveData: LiveData<T?> = _getDataLiveData
 
-
     fun saveData(value: T, successSave: ((Boolean) -> Unit)? = null) {
-        db.getReference(getTableName()).setValue(value) { error, _ ->
-            successSave?.invoke(error == null)
-            error?.let {
-                Log.e("FRDWrapper", it.message)
+        db.getReference(getTableName()).setValue(value)
+            .addOnCompleteListener { task ->
+                successSave?.invoke(task.isSuccessful)
+                if (!task.isSuccessful) {
+                    Log.e("FRDWrapper", "Error saving data", task.exception)
+                }
             }
-        }
     }
-
 
     fun getData() {
-        db.getReference(getTableName()).get().addOnSuccessListener {
-            _getDataLiveData.postValue(it.getValue(getClassType()))
+        db.getReference(getTableName()).get().addOnSuccessListener { snapshot ->
+            _getDataLiveData.postValue(snapshot.getValue(getClassType()))
+        }.addOnFailureListener {
+            Log.e("FRDWrapper", "Error getting data", it)
         }
     }
-
-
-
-
 }
