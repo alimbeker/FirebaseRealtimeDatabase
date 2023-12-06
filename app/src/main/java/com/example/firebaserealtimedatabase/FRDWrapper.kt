a465e6f10279475b7249c8c101717fec68376403
Amir Alimbekerov <200103054@stu.sdu.edu.kz>
2023-12-06 19:32:51 +0600

    add LiveData

--- app/src/main/java/com/example/firebaserealtimedatabase/FRDWrapper.kt
+++ app/src/main/java/com/example/firebaserealtimedatabase/FRDWrapper.kt
@@ -1,12 +1,18 @@
 package com.example.firebaserealtimedatabase
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.FirebaseDatabase
 
 abstract class FRDWrapper<T> {
     private val db = FirebaseDatabase.getInstance()
 
     protected abstract fun getTableName(): String
+
+    private val _getDataLiveData = MutableLiveData<T?>()
+    val getDataLiveData: LiveData<T?> = _getDataLiveData
+
 
     fun saveData(value: T, successSave: ((Boolean) -> Unit)? = null) {
         db.getReference(getTableName()).setValue(value) { error, _ ->
